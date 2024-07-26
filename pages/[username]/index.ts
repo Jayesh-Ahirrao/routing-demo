36773d1921f2cdade873898b978f9448b453d0fe
Jayesh <en001507@gmail.com>
2024-07-26 19:53:54 +0530

    Dynamic route using [] which can be file or folder and can be accessed via useRouter().query.[name]

---
+++ pages/[username]/index.ts
@@ -0,0 +1,8 @@
+// will hit on localhost:3000/[username] 
+// will hit on localhost:3000/jayesh 
+
+// import { useRouter } from "next/router"
+
+// const router = useRouter();
+
+// const username = router.query.username;
