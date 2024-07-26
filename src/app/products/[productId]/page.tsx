4875d55abff5c7028f9c244c27831aee78309a38
Jayesh <en001507@gmail.com>
2024-07-26 15:06:24 +0530

    Dynamic route using  [name] ---> folder

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>Dynamic route inside product</h1>
+  )
+}
+
+export default page;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <h1>Product list</h1>
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+            </ul>
+        </>
+    )
+}
+
+export default page
