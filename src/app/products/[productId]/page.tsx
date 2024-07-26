70e39cf118ba76c0795b2a28062100333da63338
Jayesh <en001507@gmail.com>
2024-07-26 15:12:17 +0530

    defined type of params

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = ({params}) => {
+const page = ({params} : {params: {productId : string}}) => {   // to remove TS error 
   return (
     <h1>Dynamic route inside product {params.productId}</h1>
   )
